d68f6349e31a21e50e0b7158f25767cd9bf338e6
Jofiel Batista <jofielbatista@hotmail.com>
2025-05-02 23:22:09 +0000

    video updates

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+  // Method to add two numbers
+  public int sum(int a, int b) {
+    return a + b;
+  }
+
+  // Method to subtract one number from another
+  public int subtract(int a, int b) {
+    return 0;
+  }
+}
